e46d48c4460f0ab6ec2ecb6bfd5fd8a97c28313a
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-08 23:12:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,9 +17,9 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    position_1 = board(win_combination[0])
-    position_2 = board(win_combination[1])
-    position_3 = board(win_combination[2])
+    win_index_1 = board(win_combination[0])
+    win_index_2 = board(win_combination[1])
+    win_index_3 = board(win_combination[2])
 
     if position_1 == 'X' && position_2 == 'X' && position_3 == 'X'
       return win_combination
