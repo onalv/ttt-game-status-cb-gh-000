0ce40e122b587403ad4df27f60bbe6700a1f90ff
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-08 23:38:19 +0000

    automatically backed up by learn

--- lib/test.rb
+++ lib/test.rb
@@ -10,12 +10,13 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
+  print WIN_COMBINATIONS
   WIN_COMBINATIONS.each do |win_combination|
     print win_combination
     # win_index_1 = win_combination[0]
     # win_index_2 = win_combination[1]
     # win_index_3 = win_combination[2]
-    # 
+    #
     # position_1 = board[win_index_1]
     # position_2 = board[win_index_2]
     # position_3 = board[win_index_3]
