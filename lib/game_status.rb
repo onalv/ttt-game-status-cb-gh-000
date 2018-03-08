947b9ac1bf400a68d4d4bf0bdcfec33e2603f99a
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-08 23:26:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,10 +25,6 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
-    if win_combination = [3,4,5]
-      puts win_combination + win_index_1 + win_index_2 + win_index_3 + position_1 + position_2 + position_3
-    end
-
     if position_1 == position_2 && position_2 == position_3
       return win_combination
     else
