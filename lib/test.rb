408a43454437c32192baaa0c302ad9573857ed39
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-08 23:42:17 +0000

    automatically backed up by learn

--- lib/test.rb
+++ lib/test.rb
@@ -15,11 +15,11 @@ def won?(board)
       win_index_1 = win_combination[0]
       win_index_2 = win_combination[1]
       win_index_3 = win_combination[2]
-      
+
       position_1 = board[win_index_1]
       position_2 = board[win_index_2]
       position_3 = board[win_index_3]
-      return win_combination
+      return position_1
     end
     # win_index_1 = win_combination[0]
     # win_index_2 = win_combination[1]
