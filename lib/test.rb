860bfe764bc17152be7650d40a5641538d4b7d39
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-08 23:47:12 +0000

    automatically backed up by learn

--- lib/test.rb
+++ lib/test.rb
@@ -23,7 +23,7 @@ def won?(board)
       if position_1 == position_2 && position_2 == position_3
         return win_combination
       else
-        false
+        return false
       end
     end
     # win_index_1 = win_combination[0]
