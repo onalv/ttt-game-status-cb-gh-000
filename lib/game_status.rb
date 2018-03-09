0f68ab8925989c17df98cc375be2b2b5f70c8073
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-09 00:26:22 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,22 +26,18 @@ def won?(board)
   end
 end
 
-#full?
 def full?(board)
   board.all?{|occupied| occupied != " "}
 end
 
-#draw?
 def draw?(board)
   !(won?(board)) && (full?(board))
 end
 
-#over?
 def over?(board)
   (won?(board)) || (full?(board)) || (draw?(board))
 end
 
-#winner?
 def winner(board)
   WIN_COMBINATIONS.detect do |win_combo|
     if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
